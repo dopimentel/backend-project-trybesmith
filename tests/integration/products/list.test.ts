f9d8d3c3756e7160cd6895160552c247873ffaab
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-09-30 15:21:24 -0300

    feat: req02

--- tests/integration/products/list.test.ts
+++ tests/integration/products/list.test.ts
@@ -1,10 +1,22 @@
 import sinon from 'sinon';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import ProductModel from '../../../src/database/models/product.model';
+import app from '../../../src/app';
+import productMock from '../../mocks/product.mock';
 
 chai.use(chaiHttp);
 
 describe('GET /products', function () { 
   beforeEach(function () { sinon.restore(); });
+
+  it('should return status 200 and an array of products', async function () {
+    const mockFindAllReturn = [ProductModel.build(productMock.validProduct)];
+    sinon.stub(ProductModel, 'findAll').resolves(mockFindAllReturn);
+    const response = await chai.request(app).get('/products');
+
+    expect(response.status).to.equal(200);
+    expect(response.body).to.deep.equal([productMock.validProduct]);
+  });
 
 });
