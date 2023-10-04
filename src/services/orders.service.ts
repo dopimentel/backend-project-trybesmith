14c2031af8f855efcfd59e4cb63ffe383781b525
Marcos Pimentel <mpc_marcos@hotmail.com>
2023-10-03 21:59:43 -0300

    feat: req06 - refatoração - transaction

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,8 +1,12 @@
+import { Sequelize } from 'sequelize';
 import OrderModel,
 { GetAllOrderReturn, OrderBody } from '../database/models/order.model';
 import ProductModel from '../database/models/product.model';
 import UserModel from '../database/models/user.model';
 import validateOrder from './validations/order.validation';
+import config = require('../database/config/database');
+
+const sequelize = new Sequelize(config);
 
 async function getAllWithProducts(): Promise<GetAllOrderReturn> {
   const orders = await OrderModel.findAll();
@@ -21,23 +25,24 @@ async function getAllWithProducts(): Promise<GetAllOrderReturn> {
 }
 
 async function create(orderData: OrderBody): Promise<OrderBody | undefined> {
-  validateOrder(orderData);
   const { userId, productIds } = orderData;
+  validateOrder(orderData);
   const user = await UserModel.findOne({ where: { id: userId } });
-  if (!user) {
-    return undefined;
-  }
-
-  const order = await OrderModel.create({ userId });
-  const orderId = order.dataValues.id;
-  const products = await ProductModel.findAll({
-    where: {
-      id: productIds,
-    },
+  if (!user) return undefined;
+  await sequelize.transaction(async (t) => {
+    const order = await OrderModel.create({ userId }, { transaction: t });
+    const orderId = order.dataValues.id;
+    const products = await ProductModel.findAll({
+      where: {
+        id: productIds,
+      },
+      transaction: t,
+    });
+    const productsPromises = products
+      .map((product) => product.update({ orderId }, { transaction: t }));
+    await Promise.all(productsPromises);
   });
-  const productsPromises = products.map((product) => product.update({ orderId }));
-  await Promise.all(productsPromises);
-  return { userId, productIds };
+  return orderData;
 }
 
 export default {
